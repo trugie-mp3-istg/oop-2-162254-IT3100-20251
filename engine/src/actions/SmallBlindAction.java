a9201d7949a3520000e81d5f4a9e5531737993ca
davebui306 <tranducvietbui@gmail.com>
2025-11-24 18:29:05 +0700

    Implement action classes for poker game: AllInAction, BetAction, BigBlindAction, CallAction, CheckAction, FoldAction, RaiseAction, and SmallBlindAction

---
+++ engine/src/actions/SmallBlindAction.java
@@ -0,0 +1,15 @@
+package actions;
+
+/**
+ * Hành động đặt tiền mù nhỏ (Small Blind).
+ */
+public class SmallBlindAction extends Action {
+
+    /**
+     * Hàm khởi tạo.
+     */
+    /* package */ SmallBlindAction() {
+        super("Small blind", "posts the small blind");
+    }
+    
+}
